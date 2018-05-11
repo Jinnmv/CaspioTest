416217f7c7e2ad92e9d574ed969d9b16177124b0
Anton S <jin.wishmaster@gmail.com>
2018-05-11 17:49:19 +0300

    Update main.js

--- WCAG/js/main.js
+++ WCAG/js/main.js
@@ -1,30 +1,46 @@
 
 (function() {
   
-  var locale_file = 'en.json';
+    var locale_file = 'en.json';
   
-  function loadJSON(callback) {
+    function loadJSON(callback) {
 
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', locale_file, true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == 200) {
-
-            // .open will NOT return a value but simply returns undefined in async mode so use a callback
-            callback(xobj.responseText);
-
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', locale_file, true);
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState == 4 && xobj.status == 200) {
+                // .open will NOT return a value but simply returns undefined in async mode so use a callback
+                callback(xobj.responseText);
+            }
         }
+        xobj.send(null);
     }
-    xobj.send(null);
 
-}
-
+    function jsonToTable(json) {
+        var results = JSON.parse(json);
+        
+        // EXTRACT VALUE FOR HTML HEADER
+        var columns = [];
+        for (record in results) {
+            for (field in record) {
+                if (columns.indexOf(field) === -1) {
+                    columns.push(field);
+                }
+            }
+        }
+        return columns;
+    }
+  
+  
 // Call to function with anonymous callback
 loadJSON(function(response) {
     // Do Something with the response e.g.
     //jsonresponse = JSON.parse(response);
     console.log('json loaded');
+    
+    console.log(jsonToTable(response));
+    
     // Assuming json data is wrapped in square brackets as Drew suggests
     //console.log(jsonresponse[0].name);
 
